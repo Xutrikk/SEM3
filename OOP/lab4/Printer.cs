1e1fe15d2785d7477b31d45d2f9ba03384476e1d
Kirill <153691801+Xutrikk@users.noreply.github.com>
2024-12-28 19:09:34 +0300

    Add files via upload

---
+++ OOP/lab4/Printer.cs
@@ -0,0 +1,29 @@
+﻿using lab_04;
+using System;
+
+namespace lab_04
+{
+    /*Создайте дополнительный класс Printer c полиморфным методом
+IAmPrinting(SomeAbstractClassorInterface someobj). Формальным
+параметром метода должна быть ссылка на абстрактный класс или наиболее
+общий интерфейс в вашей иерархии классов.В методе iIAmPrinting
+определите тип объекта и вызовите ToString(). .*/
+    class Printer
+    {
+        public void IAmPrinting(Inventory inventory)
+        {
+            Console.WriteLine(inventory.ToString());
+        }
+    }
+}
+interface B
+{
+    void Function();
+}
+class A:B
+{
+    A name = new A();
+    void B.Function() {
+        Console.WriteLine("Dsad");
+    }
+}
