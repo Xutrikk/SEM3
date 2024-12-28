017f5a437793650c438dffdac7d5658bb460fcd9
Kirill <153691801+Xutrikk@users.noreply.github.com>
2024-12-28 19:13:50 +0300

    Add files via upload

---
+++ OOP/lab12/HKVFileManager.cs
@@ -0,0 +1,69 @@
+﻿using System.IO.Compression;
+using System.IO;
+using System.Linq;
+
+namespace Lab12
+{
+    /*5. Создать класс XXXFileManager. Набор методов определите самостоятельно. С его помощью выполнить следующие действия:
+        a. Прочитать список файлов и папок заданного диска. Создать директорий XXXInspect, создать текстовый файл 
+        xxxdirinfo.txt и сохранить туда информацию. Создать копию файла и переименовать его. Удалить первоначальный файл.
+        b. Создать еще один директорий XXXFiles. Скопировать в него все файлы с заданным расширением из заданного 
+        пользователем директория. Переместить XXXFiles в XXXInspect.
+        c. Сделайте архив из файлов директория XXXFiles. Разархивируйте его в другой директорий.*/
+    public static class HKVFileManager
+    {
+        public static void InspectDriver(string driverName)
+        {
+            Directory.CreateDirectory(@"C:\BSTU\SEM3\OOP\lab12oop\lab12oop\HKVInspect");
+            File.Create(@"C:\BSTU\SEM3\OOP\lab12oop\lab12oop\HKVInspect\HKVdirinfo.txt").Close();
+            var currentDrive = DriveInfo.GetDrives().Single(x => x.Name == driverName);
+
+            using (StreamWriter file = new StreamWriter(@"C:\BSTU\SEM3\OOP\lab12oop\lab12oop\HKVInspect\HKVdirinfo.txt"))
+            {
+                file.WriteLine("Список папок:");
+                foreach (var s in currentDrive.RootDirectory.GetDirectories())
+                {
+                    file.WriteLine(s);
+                }
+                file.WriteLine("Список файлов:");
+                foreach (var f in currentDrive.RootDirectory.GetFiles())
+                {
+                    file.WriteLine(f);
+                }
+            }
+
+            File.Copy(@"C:\BSTU\SEM3\OOP\lab12oop\lab12oop\HKVInspect\HKVdirinfo.txt", @"C:\BSTU\SEM3\OOP\lab12oop\lab12oop\HKVInspect\HKVdirinfoCopy.txt", true);
+            File.Delete(@"C:\BSTU\SEM3\OOP\lab12oop\lab12oop\HKVInspect\HKVdirinfo.txt");
+        }
+
+        public static void CopyFiles(string path, string extention)
+        {
+            Directory.CreateDirectory(@"C:\BSTU\SEM3\OOP\lab12oop\lab12oop\HKVFiles");
+            DirectoryInfo directory = new DirectoryInfo(path);
+            DirectoryInfo directory2 = new DirectoryInfo(@"C:\BSTU\SEM3\OOP\lab12oop\lab12oop\HKVInspect\HKVFiles");
+            foreach (var f in directory.GetFiles())
+            {
+                if (f.Extension == extention)
+                    f.CopyTo(@"C:\BSTU\SEM3\OOP\lab12oop\lab12oop\HKVFiles\" + f.Name + extention, true);
+            }
+            if (!directory2.Exists)
+                Directory.Move(@"C:\BSTU\SEM3\OOP\lab12oop\lab12oop\HKVFiles", @"C:\BSTU\SEM3\OOP\lab12oop\lab12oop\HKVInspect\HKVFiles");
+            else
+                Directory.Delete(@"C:\BSTU\SEM3\OOP\lab12oop\lab12oop\HKVFiles", true);
+        }
+
+        public static void CreateArchive(string dir)
+        {
+            string name = @"C:\BSTU\SEM3\OOP\lab12oop\lab12oop\HKVInspect\HKVFiles";
+            if (new DirectoryInfo(@"C:\BSTU\SEM3\OOP\lab12oop\lab12oop\HKVInspect").GetFiles("*.zip").Length == 0)
+            {
+                ZipFile.CreateFromDirectory(name, name + ".zip");
+                DirectoryInfo direct = new DirectoryInfo(dir);
+                foreach (var innerFile in direct.GetFiles())
+                    innerFile.Delete();
+                direct.Delete();
+                ZipFile.ExtractToDirectory(name + ".zip", dir);
+            }
+        }
+    }
+}
---
+++ OOP/lab12/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Lab12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("*****************************************************************************************************");
+                HKVDiskInfo.WriteDiskInfo("C:\\");
+                HKVLog.WriteInLog("HKVDiskInfo.getFreeDrivesSpace()");
+
+                Console.WriteLine("******************************************************************************************************");
+                HKVFileInfo.WriteFileInfo(@"C:\BSTU\SEM3\OOP\lab12oop\12_Потоки_файловая система.pdf");
+                HKVLog.WriteInLog("HKVFileInfo.WriteFileInfo()", "HKVLogfile.txt", @"C:\BSTU\SEM3\OOP\lab12oop\lab12oop\HKVLogfile.txt");
+
+                Console.WriteLine("******************************************************************************************************");
+                HKVDirInfo.WriteDirInfo(@"C:\BSTU\SEM3\OOP");
+                HKVLog.WriteInLog("HKVDirInfo.WriteDirInfo()", @"C:\BSTU\SEM3\OOP");
+
+                HKVFileManager.InspectDriver("C:\\");
+                HKVLog.WriteInLog("HKVFileManager.InspectDriver()", "C:\\");
+                HKVFileManager.CopyFiles(@"C:\BSTU\SEM3\KPO", ".docx");
+                HKVLog.WriteInLog("HKVFileManager.CopyFiles()", @"C:\BSTU\SEM3\KPO");
+                HKVFileManager.CreateArchive(@"C:\BSTU\SEM3\OOP\lab12oop\lab12oop\HKVARCHIVE");
+                HKVLog.WriteInLog(" HKVFileManager.CreateArchive()");
+
+                HKVLog.FindInfo();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
